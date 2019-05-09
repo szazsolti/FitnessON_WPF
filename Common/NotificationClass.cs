d833b5841905391ef96d358b8bf13b7154b4f87a
Szabo Zsolt <szazsolti10@gmail.com>
2019-05-09 11:26:58 +0300

    First commit.

---
+++ Common/NotificationClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMApplication.Infra
+{
+    public class NotificationClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        // = delegate { }
+        public void OnProprtyChanged([CallerMemberName]string propertyName = null)
+        {
+            //PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
