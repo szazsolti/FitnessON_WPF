9ade90606861d9e7f3cf114e4df37aee485f9483
killersolyom <killersolyom@gmail.com>
2019-05-10 17:10:41 +0300

    LeaseSelector screen implemented

--- View/LeaseSelector.xaml.cs
+++ View/LeaseSelector.xaml.cs
@@ -1,6 +1,4 @@
-﻿using FitnessON.Infra;
-using FitnessON.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FitnessON
+namespace FitnessON.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LeaseSelector.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LeaseSelector : UserControl
     {
-        public MainWindow()
+        public LeaseSelector()
         {
             InitializeComponent();
-            
         }
     }
 }
