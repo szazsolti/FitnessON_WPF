2d21828f52d8d7a3b0abc36410d3cd95f1edb8ae
Szabo Zsolt <szazsolti10@gmail.com>
2019-05-12 15:57:14 +0300

    Working List users from admin menu.

--- ViewModel/UserListingViewModel.cs
+++ ViewModel/UserListingViewModel.cs
@@ -5,11 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 using FitnessON.Common;
 using FitnessON.Infra;
+using FitnessON.Logic;
+using FitnessON.Model;
 
 namespace FitnessON.ViewModel
 {
     public class UserListingViewModel : NotificationClass, IUserListingContent
     {
+        public List<User> users=new List<User>();
         public string Header => "Ügyfél keresés";
+
+        public UserListingViewModel()
+        {
+            GetUsers();
+            this.RefreshUsers = new RelayCommand(this.RefreshUsersExecute);
+        }
+
+        public List<User> Users
+        {
+            get
+            {
+                return this.users;
+            }
+            set
+            {
+                this.users = value;
+                this.OnProprtyChanged();
+            }
+        }
+
+        public void GetUsers()
+        {
+            this.users = Data.Controller.GetUsers();
+        }
+
+
+        public RelayCommand RefreshUsers
+        {
+            get;
+            set;
+        }
+
+        public void RefreshUsersExecute()
+        {
+            GetUsers();
+        }
+
     }
 }
